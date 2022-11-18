db99f16ec5617bae4f6043fc4d332a6acdab98a2
wadocode <art.wado@gmail.com>
2022-11-17 21:45:00 -0500

    app actualizado

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey:'618b55090366aeab27c8e27808c2db1b',
+    apiKey:'tu api key',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
